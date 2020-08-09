da04c0828c5d3e278968a0e77297b02f849e5d21
Abhijit Ghatnekar <aghatnekar@qualys.com>
2020-08-09 22:10:41 +0530

    Corner cases for absentees

--- Databases/Joe Celko's SQL Puzzles/2_Absentees.sql
+++ Databases/Joe Celko's SQL Puzzles/2_Absentees.sql
@@ -51,6 +51,25 @@ DELETE FROM Personnel WHERE emp_id IN (
     HAVING SUM(severity_points) >= 40
 )
 
+/* 
+Above query does not disregard weekends or calendar holidays. If an employee takes a sick leave that overlaps with a weekend, the weekend also gets counted in the sick
+leave. To prevent that, let's assume that there is a calendar table 
+*/
+
+/* 2.1 Calendar table tracks the days of an year and holidays and weekends. */
+
+CREATE TABLE calendar (
+    cal_date DATE NOT NULL PRIMARY KEY,
+    date_type CHAR(20) CHECK (date_type IN 'WEEKEND', 'WORK-DAY', 'HOLIDAY')
+);
+
+DELETE FROM Personnel WHERE emp_id IN (
+    SELECT ABSENTEEISM.emp_id
+    FROM ABSENTEEISM INNER JOIN calendar ON ABSENTEEISM.cal_date = calendar.cal_date
+    WHERE absentee_date BETWEEN DATE(NOW() - interval '1 YEAR') AND DATE(NOW()) AND calendar.date_type='WORK-DAY'
+    GROUP BY ABSENTEEISM.emp_id
+    HAVING SUM(severity_points) >= 40
+)
 
 /*** 
 3. If an employee is absent for more than one day in a row, its termed as a long-term illness. He/She does not accrue any point for the second, third and later days nor do they
@@ -63,6 +82,23 @@ WHERE EXISTS (
     WHERE A1.emp_id = A2.emp_id AND A1.absentee_date = A2.absentee_date - INTERVAL '1 DAY'
 );
 
+/* 
+Above query does not disregard weekends or calendar holidays. If an employee takes a sick leave that overlaps with a weekend, the weekend also gets counted in the sick
+leave. To prevent that, let's assume that there is a calendar table 
+*/
+
+/* 3.1 Calendar table tracks the days of an year and holidays and weekends. */
+
+CREATE TABLE calendar (
+    cal_date DATE NOT NULL PRIMARY KEY,
+    date_type CHAR(20) CHECK (date_type IN 'WEEKEND', 'WORK-DAY', 'HOLIDAY')
+);
+
+UPDATE ABSENTEEISM SET severity_points=0, reason_code='long-term illness'
+WHERE EXISTS (
+    SELECT * FROM ABSENTEEISM A1, calendar C1 
+    WHERE  A1.absentee_date = C1.cal_date - INTERVAL '1 DAY' AND C1.date_type='WORK-DAY'
+);
 
 /** 
 4. We need to add on ON DELETE CASCADE to Personnel table. This is because in the delete query in point 2, we are deleting data from personnel table for offending employees
