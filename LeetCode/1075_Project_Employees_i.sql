04e956245238900c339b1c480296f15d49455910
Abhijit Ghatnekar <abhighat@yahoo.co.in>
2021-10-18 09:31:54 +0530

    SQL

---
+++ LeetCode/1075_Project_Employees_i.sql
@@ -0,0 +1,5 @@
+SELECT project_id, ROUND(AVG(experience_years), 2) AS average_years 
+FROM project 
+JOIN Employee ON project.employee_id = employee.employee_id
+GROUP BY project_id
+ORDER BY project_id
